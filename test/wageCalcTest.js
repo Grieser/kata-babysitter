12bceecde2a372949acc1426d40f497cf067cdc0
Eric <uigrieser@gmail.com>
2018-08-26 14:36:48 -0500

    Added test to prevent job ending after 4:00 AM

--- test/wageCalcTest.js
+++ test/wageCalcTest.js
@@ -11,4 +11,9 @@ describe('wageCalc', function(){
         let result = getWage.getWage('4:59 PM', '7:00 PM', '9:00 PM');
         assert.equal( result, 'Error');
     })
+
+    it('Ending time should not be after 4:00 AM', function(){
+        let result = getWage.getWage('5:00 PM', '7:00 PM', "4:01 AM")
+        assert.equal(result, "Error");
+    })
 });
--- wageCalc.js
+++ wageCalc.js
@@ -4,8 +4,10 @@ moment().format();
 module.exports = {
     getWage: function(startJob, bedTime, endJob){
         var start = moment(startJob, "HH:mm A");
+        var end = moment(endJob, "HH:mm A");
         var startLimit = moment("5:00 PM", "HH:mm A");
-        if (start.isBefore(startLimit)) {
+        var endLimit = moment("4:00 AM", "HH:mm A");
+        if (start.isBefore(startLimit) || end.isAfter(endLimit)) {
             return "Error";
         }
         return "$0";
