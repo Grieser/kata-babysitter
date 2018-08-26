81ea0d9a46c479e22163e172a0c67f97a4faf74d
Eric <uigrieser@gmail.com>
2018-08-26 14:28:10 -0500

    Added test to prevent job starting before 5:00 PM

--- test/wageCalcTest.js
+++ test/wageCalcTest.js
@@ -6,4 +6,9 @@ describe('wageCalc', function(){
         let result = getWage.getWage('5:00 PM', '7:00 PM', '9:00 PM');
         assert.typeOf(result, "string");
     })
+
+    it('Start Time should not be before 5:00 PM', function() {
+        let result = getWage.getWage('4:59 PM', '7:00 PM', '9:00 PM');
+        assert.equal( result, 'Error');
+    })
 });
--- wageCalc.js
+++ wageCalc.js
@@ -1,5 +1,13 @@
+var moment = require('moment');
+moment().format();
+
 module.exports = {
     getWage: function(startJob, bedTime, endJob){
+        var start = moment(startJob, "HH:mm A");
+        var startLimit = moment("5:00 PM", "HH:mm A");
+        if (start.isBefore(startLimit)) {
+            return "Error";
+        }
         return "$0";
     }
 }
